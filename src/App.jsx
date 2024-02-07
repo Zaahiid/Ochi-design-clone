f6ed0c78ce109351e1d2394e657b05efea868d1a
Zaahiid <zahidrsheikh01@gmail.com>
2024-02-07 23:59:33 +0530

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-  <div className="flex justify-center items-center text-white bg-black/70 h-screen font-bold text-5xl">
-    <h1>Ochi Design Clone</h1>
+  <div className="w-full h-screen bg-zinc-900 text-white">
+  <Navbar/>
   </div>
   )
 }
