81dd3aba92d2bd097ef12c85665aa09d452c7a06
Zaahiid <zahidrsheikh01@gmail.com>
2024-02-11 00:44:29 +0530

    Added Marquee and installed Framer motion

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,14 @@
+
+const Marquee = () => {
+  return (
+    <div className='w-full py-20 rounded-t-3xl bg-[#004d43]'>
+    <div className='flex gap-10 overflow-hidden whitespace-nowrap text border-t-2 border-b-2 border-zinc-300'>
+        <h1 className='text-[18vw] leading-none font-founders-grotesk font-bold uppercase mb-3' >We are ochi</h1>
+        <h1 className='text-[18vw] leading-none font-founders-grotesk font-bold uppercase mb-3' >We are ochi</h1>
+    </div>
+
+    </div>
+  )
+}
+
+export default Marquee
