8f25c252438ad03ef5e86f2b08be79b13cbb8901
Zaahiid <zahidrsheikh01@gmail.com>
2024-02-13 23:59:32 +0530

    eyes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import LandingPage from "./components/LandingPage"
 import Marquee from "./components/Marquee"
 import About from "./components/About"
+import Eyes from "./components/Eyes"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
   <LandingPage/>
   <Marquee/>
   <About/>
+  <Eyes/>
   </div>
   )
 }
---
+++ src/components/Eyes.jsx
@@ -0,0 +1,14 @@
+
+const Eyes = () => {
+  return (
+    <div className="eyes w-full h-screen overflow-hidden">
+    <div className="relative w-full h-full  bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]">
+        <div className="absolute w-1/3 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] bg-red-500">
+            <div className="w-[15vw] h-[15vw] bg-red-300 rounded-full"></div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Eyes
